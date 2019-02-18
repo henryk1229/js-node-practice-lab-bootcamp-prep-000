03eb67d68edc4c74306ed12af2484055cc0ce520
Henry Koehler <henryk1229@gmail.com>
2019-02-18 22:36:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-// write your code below!
+var name = 'Henry'
+var height = '68'
 
-var name = "Henry"
-var height = 68
+console.log('Name:'); 
+console.log(name); 
 
-console.log("Name:")
-console.log(name)
-
-console.log("Height:")
-console.log(height+1)
+console.log('Height:');
+console.log(height + 1);
